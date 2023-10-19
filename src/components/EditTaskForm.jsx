c0b64ebbf200c712580ad264f59cc15037f826cf
michel sampil <michel.sampil@gmail.com>
2023-10-19 18:54:31 -0300

    adding modal and form

---
+++ src/components/EditTaskForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export const EditTaskForm = ({ task, onEdit }) => {
+  const [editedDescription, setEditedDescription] = useState(task.description);
+  const [isCompleted, setIsCompleted] = useState(task.isCompleted);
+
+  const handleSaveClick = () => {
+    onEdit({
+      ...task,
+      description: editedDescription,
+      isCompleted: isCompleted,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        width: "30vw",
+        height: "23vh",
+        backgroundColor: "#242424",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+        }}
+      >
+        <h2>TASK EDITION</h2>
+        <label style={{ display: "flex", justifyContent: "start" }}>
+          Task Description
+        </label>
+        <input
+          type="text"
+          value={editedDescription}
+          onChange={(e) => setEditedDescription(e.target.value)}
+        />
+        <div style={{ display: "flex", justifyContent: "start" }}>
+          <label style={{ display: "flex", justifyContent: "start" }}>
+            {" "}
+            Completed:{" "}
+          </label>
+          <input
+            type="checkbox"
+            checked={isCompleted}
+            onChange={() => setIsCompleted(!isCompleted)}
+          />
+        </div>
+
+        <button onClick={handleSaveClick}>Save</button>
+      </div>
+    </div>
+  );
+};
